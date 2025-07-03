423a77db41cf0a3a37fbf8c6972c104c08fefa17
revenuflo <oussama@revenuflo.com>
2025-07-03 17:38:08 +0100

    Create svg_injector.js

---
+++ svg_injector.js
@@ -0,0 +1,8 @@
+(function loadSVGInjector() {
+  const script = document.createElement("script");
+  script.src = "https://cdnjs.cloudflare.com/ajax/libs/svg-injector/1.1.3/svg-injector.min.js";
+  script.integrity = "sha512-LpKoEmPyokcDYSjRJ/7WgybgdAYFsKtCrGC9m+VBwcefe1vHXyUnD9fTQb3nXVJda6ny1J84UR+iBtEYm3OQmQ==";
+  script.crossOrigin = "anonymous";
+  script.referrerPolicy = "no-referrer";
+  document.head.appendChild(script);
+})();
